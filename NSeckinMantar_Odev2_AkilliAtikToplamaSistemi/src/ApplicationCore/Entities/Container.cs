482482a790e67cbd10bfd0475c8259d0bc58358f
NseckinM <seckinmantar@gmail.com>
2022-01-04 00:19:26 +0300

    ApplicationCore layer done

---
+++ NSeckinMantar_Odev2_AkilliAtikToplamaSistemi/src/ApplicationCore/Entities/Container.cs
@@ -0,0 +1,20 @@
+﻿using ApplicationCore.Common;
+using System;
+
+namespace ApplicationCore.Entities
+{
+    public class Container : BaseEntity
+    {
+
+        public string ContainerName { get; set; }
+
+        public Decimal Latitude { get; set; }
+
+        public Decimal Longitude { get; set; }
+
+
+        public int VehicleId { get; set; }
+        public Vehicle Vehicle { get; set; }
+
+    }
+}
---
+++ NSeckinMantar_Odev2_AkilliAtikToplamaSistemi/src/ApplicationCore/Interfaces/UnitOfWork/IUnitOfWork.cs
@@ -0,0 +1,19 @@
+﻿using ApplicationCore.Interfaces.Repository;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ApplicationCore.Interfaces.UnitOfWork
+{
+    public interface IUnitOfWork : IDisposable
+    {
+        IContainerRepository Container { get; }
+
+        IVehicleRepository Vehicle { get; }
+
+        int Complete();
+
+    }
+}
